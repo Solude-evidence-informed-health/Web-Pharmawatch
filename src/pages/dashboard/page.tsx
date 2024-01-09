6e6763ea1c4a366f55f80e5b6af5ec7fc85b6baa
Victor Hugo <vhsouza606@gmail.com>
2024-01-09 19:22:40 -0300

    feat: página dashboard

---
+++ src/pages/dashboard/page.tsx
@@ -0,0 +1,59 @@
+import { useStyles } from 'bold-ui'
+
+export const DashboardPage = () => {
+  const { css, theme } = useStyles()
+  return (
+    <section
+      className={css({
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: '4fr 1fr',
+        gap: 24,
+      })}
+    >
+      <div
+        className={css({
+          padding: 164,
+          backgroundColor: theme.pallete.surface.main,
+          borderRadius: theme.radius.paper,
+          border: `1px solid ${theme.pallete.divider}`,
+          backgroundImage: `repeating-linear-gradient(45deg, 
+            ${theme.pallete.gray.c90} 10px,
+            ${theme.pallete.gray.c90} 12px,
+            transparent 12px,
+            transparent 24px);
+        `,
+        })}
+      />
+      <div
+        className={css({
+          padding: 164,
+          backgroundColor: theme.pallete.surface.main,
+          borderRadius: theme.radius.paper,
+          border: `1px solid ${theme.pallete.divider}`,
+          backgroundImage: `repeating-linear-gradient(45deg, 
+            ${theme.pallete.gray.c90} 10px,
+            ${theme.pallete.gray.c90} 12px,
+            transparent 12px,
+            transparent 24px);
+        `,
+        })}
+      />
+      <div
+        className={css({
+          padding: 164,
+          backgroundColor: theme.pallete.surface.main,
+          borderRadius: theme.radius.paper,
+          border: `1px solid ${theme.pallete.divider}`,
+          backgroundImage: `repeating-linear-gradient(45deg, 
+            ${theme.pallete.gray.c90} 10px,
+            ${theme.pallete.gray.c90} 12px,
+            transparent 12px,
+            transparent 24px);
+        `,
+          gridColumn: 'span 2',
+        })}
+      />
+    </section>
+  )
+}
